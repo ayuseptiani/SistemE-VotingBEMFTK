b92c3a244a18f9ba7737eda054e6c3be169eca66
ayuseptiani <152187896+ayuseptiani@users.noreply.github.com>
2024-11-28 00:23:28 +0700

    Add files via upload

---
+++ Sistem_EVoting_BEMFTK.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistem_e.voting_bemftk;
+
+/**
+ *
+ * @author Ayu Septiani
+ */
+public class Sistem_EVoting_BEMFTK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
